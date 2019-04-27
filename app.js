0c409957b41bb6fc12bb16e5a4ea5750396b3e30
fastEnergyBot <50019200+fastEnergyBot@users.noreply.github.com>
2019-04-27 12:14:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,40 +28,44 @@ server.use(bodyParser.json());
 var app = dialogflow({clientId: '340099036889-ol3tcgb0c06poa9p6j6ijpudo7osel2j.apps.googleusercontent.com'});
 
 app.intent('Create SR',(conv,params)=>{
-	return new Promise((resolve,reject)=>{
-		
-		console.log('Value passed from google1'+params.accountName);
-		console.log('Value passed from google2'+params.typeOfSupply);
-		console.log('Value passed from google3'+params.department);
-		
-		conn.login(process.env.username, process.env.pass, function(err, res){
-			if(err){
-				
-				console.log(err);
-			}
-			else{
-				
-				//Single case record creation
-				conn.sobject("Case").create({ 
-					AccountId : '0015C00000NIcDDQA1', 
-					Status : 'New' ,
-					FE_Department__c : params.department,
-					FE_Type_of_supply__c : params.typeOfSupply 
-				},
-				function(err, ret){
-					if (err || !ret.success){ 
-					    
-						return console.error(err, ret);
-					}
-					console.log("Created record id : " + ret.id);
+	
+	
+	console.log('Value passed from google1'+params.accountName);
+	console.log('Value passed from google2'+params.typeOfSupply);
+	console.log('Value passed from google3'+params.department);
+	
+	conv.ask(new SimpleResponse({speech:"This is a test.",text:"This is a test."}));
+	
+	//conv.ask(new SimpleResponse({speech:"A new service request has been created.",text:"A new service request has been created."}));
+	
+	/*conn.login(process.env.username, process.env.pass, function(err, res){
+		if(err){
+			
+			console.log(err);
+		}
+		else{
+			
+			//Single case record creation
+			conn.sobject("Case").create({ 
+				AccountId : '0015C00000NIcDDQA1', 
+				Status : 'New' ,
+				FE_Department__c : params.department,
+				FE_Type_of_supply__c : params.typeOfSupply 
+			},
+			function(err, ret){
+				if (err || !ret.success){ 
 					
-					conv.ask(new SimpleResponse({speech:"A new service request has been created.",text:"A new service request has been created."}));
-				});
-			}
-		});
+					return console.error(err, ret);
+				}
+				console.log("Created record id : " + ret.id);
+				
+				conv.ask(new SimpleResponse({speech:"A new service request has been created.",text:"A new service request has been created."}));
+			});
+		}
+	});*/
 		
 		
-	});
+	
 });
 
 
